55762d4e5fc6af7b4fe7bb49cc625209540fd5a0
DmitriyV003 <vdmitriy003@gmail.com>
2022-07-10 20:49:41 +0300

    increment-1

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"github.com/dmitriy/alerting/internal/agent/client"
+	"github.com/dmitriy/alerting/internal/agent/service"
+)
+
+func main() {
+
+	metricService := service.New()
+	go metricService.GatherMetricsByInterval(2)
+
+	sender := client.New()
+	go sender.SendWithInterval("http://localhost:8080", &metricService.Health, 10)
+
+	select {}
+}
