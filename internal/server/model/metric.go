97b3b288256454fdfb249afa2eee7f49c7cace64
dmitriyV25 <dmitriy.vorobev@onlinesup.com>
2022-08-12 18:50:22 +0300

    refactor: make one model Metric

--- internal/server/model/metric.go
+++ internal/server/model/metric.go
@@ -3,34 +3,33 @@ package model
 const CounterType = "counter"
 const GaugeType = "gauge"
 
-func NewGauge(name string) Gauge {
-	return Gauge{
-		Metric: Metric{
-			Name: name,
-		},
-		Value: 0,
+type MetricType string
+
+func newMetric(name string) *Metric {
+	return &Metric{
+		Name: name,
 	}
 }
 
-func NewCounter(name string) Counter {
-	return Counter{
-		Metric: Metric{
-			Name: name,
-		},
-		Value: 0,
-	}
+func NewGauge(name string, val float64) Metric {
+	metric := newMetric(name)
+	metric.Type = GaugeType
+	metric.FloatValue = &val
+
+	return *metric
+}
+
+func NewCounter(name string, val int64) Metric {
+	metric := newMetric(name)
+	metric.Type = CounterType
+	metric.IntValue = &val
+
+	return *metric
 }
 
 type Metric struct {
-	Name string
-}
-
-type Gauge struct {
-	Metric
-	Value float64
-}
-
-type Counter struct {
-	Metric
-	Value int64
+	Name       string
+	Type       MetricType
+	IntValue   *int64
+	FloatValue *float64
 }
