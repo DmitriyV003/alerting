302e6ccc90da9595bc9f2cbd0b45a5f381f9ad38
dmitriyV25 <dmitriy.vorobev@onlinesup.com>
2022-08-15 17:04:19 +0300

    increment-5

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"github.com/caarlos0/env/v6"
 	"github.com/dmitriy/alerting/internal/agent/client"
 	"github.com/dmitriy/alerting/internal/agent/service"
 	log "github.com/sirupsen/logrus"
 	"os"
+	"time"
 )
+
+type config struct {
+	Address        string `env:"ADDRESS" envDefault:"localhost:8080"`
+	ReportInterval string `env:"REPORT_INTERVAL" envDefault:"10s"`
+	PollInterval   string `env:"POLL_INTERVAL" envDefault:"2s"`
+}
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{
@@ -17,12 +26,21 @@ func init() {
 }
 
 func main() {
+	var conf config
+	err := env.Parse(&conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pollInterval, _ := time.ParseDuration(conf.PollInterval)
+	reportInterval, _ := time.ParseDuration(conf.ReportInterval)
+
+	log.Infof("Agent starting. Poll interval: %s; Report interval: %s", fmt.Sprint(conf.PollInterval), fmt.Sprint(conf.ReportInterval))
 
 	metricService := service.New()
-	go metricService.GatherMetricsByInterval(2)
+	go metricService.GatherMetricsByInterval(int(pollInterval.Seconds()))
 
 	sender := client.New()
-	go sender.SendWithInterval("http://localhost:8080/update", &metricService.Health, 10)
+	go sender.SendWithInterval(fmt.Sprintf("http://%s/update", conf.Address), &metricService.Health, int(reportInterval.Seconds()))
 
 	select {}
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 )
 
 require (
+	github.com/caarlos0/env/v6 v6.9.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-chi/chi/v5 v5.0.7 // indirect
