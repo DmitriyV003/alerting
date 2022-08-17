0ae599291509ead1acacafaf62a103cf3350e7f9
dmitriyV25 <dmitriy.vorobev@onlinesup.com>
2022-08-17 15:37:02 +0300

    increment-7

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,21 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/caarlos0/env/v6"
+	"github.com/dmitriy/alerting/cmd/agent/config"
 	"github.com/dmitriy/alerting/internal/agent/client"
 	"github.com/dmitriy/alerting/internal/agent/service"
 	log "github.com/sirupsen/logrus"
 	"os"
-	"time"
 )
 
-type config struct {
-	Address        string        `env:"ADDRESS" envDefault:"localhost:8080"`
-	ReportInterval time.Duration `env:"REPORT_INTERVAL" envDefault:"10s"`
-	PollInterval   time.Duration `env:"POLL_INTERVAL" envDefault:"2s"`
-}
+var conf config.Config
 
 func init() {
+	config.ParseEnv(&conf)
 	log.SetFormatter(&log.TextFormatter{
 		DisableColors: false,
 		FullTimestamp: true,
@@ -26,11 +22,6 @@ func init() {
 }
 
 func main() {
-	var conf config
-	err := env.Parse(&conf)
-	if err != nil {
-		log.Error("Unable to parse ENV: ", err)
-	}
 	log.Infof("Agent starting. Poll interval: %s; Report interval: %s", fmt.Sprint(conf.PollInterval), fmt.Sprint(conf.ReportInterval))
 
 	metricService := service.New()
