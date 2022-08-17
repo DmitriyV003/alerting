70b62e6843971d1cedbf7caf6bddcfd9812dc209
dmitriyV25 <dmitriy.vorobev@onlinesup.com>
2022-08-17 17:38:59 +0300

    increment-8

--- internal/server/handlers/get_all_metrics.go
+++ internal/server/handlers/get_all_metrics.go
@@ -1,14 +1,20 @@
 package handlers
 
 import (
-	"encoding/json"
 	"github.com/dmitriy/alerting/internal/server/applicationerrors"
+	"github.com/dmitriy/alerting/internal/server/model"
 	"github.com/dmitriy/alerting/internal/server/storage"
+	log "github.com/sirupsen/logrus"
+	"html/template"
 	"net/http"
 )
 
 type GetAllMetricHandler struct {
 	storage storage.MetricStorage
+}
+
+type viewData struct {
+	Metrics []model.Metric
 }
 
 func NewGetAllMetricHandler(store storage.MetricStorage) *GetAllMetricHandler {
@@ -19,18 +25,15 @@ func NewGetAllMetricHandler(store storage.MetricStorage) *GetAllMetricHandler {
 
 func (h *GetAllMetricHandler) Handle(w http.ResponseWriter, r *http.Request) {
 	metrics := h.storage.GetAll()
-	metricsBytes, err := json.Marshal(metrics)
+	viewMetrics := viewData{Metrics: *metrics}
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+	ts := template.Must(template.ParseFiles("./internal/server/templates/index.gohtml"))
+	err := ts.Execute(w, viewMetrics)
 
 	if err != nil {
-		applicationerrors.WriteHTTPError(&w, http.StatusInternalServerError)
-
-		return
-	}
-
-	w.WriteHeader(http.StatusOK)
-	_, err = w.Write(metricsBytes)
-
-	if err != nil {
+		log.Error("Exec html: ", err)
 		applicationerrors.WriteHTTPError(&w, http.StatusInternalServerError)
 
 		return
