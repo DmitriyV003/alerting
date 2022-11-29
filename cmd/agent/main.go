c45e33f41a6248162329d7d1168b10c4d9c9ae79
dmitriy <vdmitriy003@gmail.com>
2022-11-29 22:44:03 +0300

    increment-15

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/rs/zerolog/log"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 type App struct {
@@ -19,6 +21,6 @@ func main() {
 		fmt.Sprint(app.conf.PollInterval),
 		fmt.Sprint(app.conf.ReportInterval),
 	)
-
+	go http.ListenAndServe(":8083", nil)
 	app.run()
 }
