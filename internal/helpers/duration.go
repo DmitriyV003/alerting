405906cc67fbebdf496819e790da518d0a2200f6
dmitriy <vdmitriy003@gmail.com>
2022-12-27 00:58:13 +0300

    increment-21

---
+++ internal/helpers/duration.go
@@ -0,0 +1,34 @@
+package helpers
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Duration struct {
+	time.Duration
+}
+
+func (dur *Duration) UnmarshalJSON(b []byte) error {
+	var unmarshalledJson interface{}
+
+	err := json.Unmarshal(b, &unmarshalledJson)
+	if err != nil {
+		return err
+	}
+
+	switch value := unmarshalledJson.(type) {
+	case float64:
+		dur.Duration = time.Duration(value)
+	case string:
+		dur.Duration, err = time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("invalid duration: %#v", unmarshalledJson)
+	}
+
+	return nil
+}
